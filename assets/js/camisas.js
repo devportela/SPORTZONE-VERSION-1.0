92a464809c1806596ef9b4ffd7d5698ff93e9f53
devportela <portelagustavo947@gmail.com>
2024-11-20 14:51:16 -0300

    .

--- assets/js/camisas.js
+++ assets/js/camisas.js
@@ -13,21 +13,3 @@ function searchProducts() {
     }
   });
 }
-// Função para alternar o estado de favorito
-function toggleFavorite(element) {
-  // Alterna a classe 'favorited' no ícone de coração
-  element.classList.toggle('favorited');
-  
-  // Verifica se o item foi favoritado
-  if (element.classList.contains('favorited')) {
-    // Se favoritado, mudar a cor para vermelho e logar no console
-    element.style.color = '#ff0000';  // Cor do ícone quando favoritado
-    console.log('Produto favoritado!');
-    
-    // Aqui você pode adicionar mais funcionalidades, como salvar no localStorage ou no banco de dados
-  } else {
-    // Se não for mais favoritado, mudar para a cor inicial e logar no console
-    element.style.color = '#ff4757';  // Cor do ícone quando não favoritado
-    console.log('Produto removido dos favoritos!');
-  }
-}
